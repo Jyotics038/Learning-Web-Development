6b46a277b7828383a89e8022ada06b1ee5a16dcb
jyoti <jyotibansal9466@gmail.com>
2021-11-30 19:30:13 +0530

    React

---
+++ props/src/components/app.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function app(){
+    return(
+        <div>
+        <div>
+        <h1>To-Do list</h1>
+        </div>
+        <div>
+        <input type="text">
+        <button>
+        <span> Add </span>
+        </button>
+        </div>
+        <div>
+    
+        <ul>
+        <li>A Item</li>
+        </ul>
+        </div>
+        </div>
+    );
+}
